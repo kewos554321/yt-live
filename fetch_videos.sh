0fd0213a144e71cf36d34c11c7be550a6b8d3309
jayson.wang <jayson.wang@cienet.com>
2025-05-23 11:57:12 +0800

    feat: Enhance video fetching: Added support for downloading files from Google Drive with confirmation token handling in fetch_videos.sh. Updated video URL in video_urls.txt to the correct format. Uncommented streaming logic in live.sh for active video streaming.

--- fetch_videos.sh
+++ fetch_videos.sh
@@ -7,8 +7,29 @@ mkdir -p "$VIDEO_DIR"
 
 i=1
 while read -r url; do
+  echo "url: $url"
   filename="$VIDEO_DIR/video_$i.mp4"
   echo "⬇️ Downloading: $url → $filename"
-  curl -L "$url" -o "$filename"
+
+  if [[ "$url" =~ drive.google.com ]]; then
+    # 取得檔案ID
+    fileid=$(echo "$url" | sed -E 's/.*\/file\/d\/([^\/]+).*/\1/')
+    echo "fileid: $fileid"
+    # 先取得確認碼
+    html=$(curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}")
+    confirm=$(echo "$html" | perl -n -e 'print "$1\n" if /confirm=([^&]+)/' | head -n 1)
+    if [[ -z "$confirm" ]]; then
+      # confirm token 沒抓到，直接嘗試下載
+      curl -Lb ./cookie "https://drive.google.com/uc?export=download&id=${fileid}" -o "$filename"
+    else
+      # 用確認碼下載檔案
+      curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=${confirm}&id=${fileid}" -o "$filename"
+    fi
+    rm -f ./cookie
+  else
+    echo "Not a Google Drive URL"
+    curl -L "$url" -o "$filename"
+  fi
+
   ((i++))
 done < "$URL_FILE"
--- live.sh
+++ live.sh
@@ -4,15 +4,15 @@ YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
 STREAM_KEY="$YT_STREAM_KEY"
 
 while true; do
-  echo "📥 Fetching videos..."
-  bash fetch_videos.sh
+  # echo "📥 Fetching videos..."
+  # bash fetch_videos.sh
 
-  # for file in ./videos/*.mp4; do
-  #   echo "▶️ Streaming $file"
-  #   ffmpeg -re -i "$file" -c:v libx264 -preset veryfast -maxrate 3000k \
-  #     -bufsize 6000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 160k \
-  #     -f flv "$YOUTUBE_URL/$STREAM_KEY"
-  # done
+  for file in ./videos/*.mp4; do
+    echo "▶️ Streaming $file"
+    ffmpeg -re -i "$file" -c:v libx264 -preset veryfast -maxrate 3000k \
+      -bufsize 6000k -pix_fmt yuv420p -g 60 -c:a aac -b:a 160k \
+      -f flv "$YOUTUBE_URL/$STREAM_KEY"
+  done
 
   echo "🔁 Restarting stream loop..."
 done
